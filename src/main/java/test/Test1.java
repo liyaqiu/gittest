f83ac82730d5da402bb626ea3b3029708edf9a17
liyaqiu <361234567@qq.com>
2022-05-07 16:39:21 +0800

    我在master改动了

---
+++ src/main/java/test/Test1.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author eric
+ * @date 2022/5/7 16:38
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("什么都没");
+
+    }
+}
